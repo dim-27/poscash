7bd7edfa78e0571c5b2bc25ed682db27a2aa05b7
Muchamad Choirul Anwar <mcnwr76@Muchamads-MacBook-Air.local>
2023-10-11 20:55:35 +0700

    merge POS-16

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,18 +1,20 @@
-import { Fragment } from "react"
-import { Outlet } from "react-router-dom"
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 
-import Header from "../layout/Header"
-import Footer from "../layout/Footer"
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
       <div className="h-screen mx-auto mt-20">
-        <Outlet />
+        <div className="w-4/5 mx-auto">
+          <Outlet />
+        </div>
       </div>
       <Footer />
     </Fragment>
-  )
-}
-export default App
+  );
+};
+export default App;

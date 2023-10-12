687b0148d77b3bfe435c8a39b07a580d59161ada
Mikhail <mikhailyuwankho@gmail.com>
2023-10-12 23:16:37 +0700

    POS-20

--- server/src/controllers/product/schema.js
+++ server/src/controllers/product/schema.js
@@ -1,18 +1,18 @@
-import joi from "joi";
+import joi from "joi"
 
 const addProduct = joi.object({
   name: joi.string().required(),
   price: joi.string().required(),
   imageUrl: joi.string().required(),
   description: joi.string().required(),
-});
+})
 
 const updateProduct = joi.object({
   name: joi.string().required(),
   price: joi.string().required(),
   imageUrl: joi.string().required(),
   description: joi.string().required(),
-});
+})
 
-const schema = { addProduct, updateProduct };
-export default schema;
+const schema = { addProduct, updateProduct }
+export default schema

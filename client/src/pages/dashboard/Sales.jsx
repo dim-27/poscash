1230746b1e077e9cf1a44dd0a4d14e940199741a
Muchamad Choirul Anwar <mcnwr76@Muchamads-MacBook-Air.local>
2023-10-14 12:25:58 +0700

    POS-23

--- client/src/pages/dashboard/Sales.jsx
+++ client/src/pages/dashboard/Sales.jsx
@@ -10,25 +10,6 @@ const Sales = () => {
     return res.data;
   });
 
-  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  // let data = [];
-  // for (let i = 0; i < 7; i++) {
-  //   const total =
-  //     isFetched &&
-  //     sales.rows
-  //       .map((item) => {
-  //         if (new Date(item.date).getDay() === i) {
-  //           const total = item.qty * item.price;
-  //           return total;
-  //         } else {
-  //           return 0;
-  //         }
-  //       })
-  //       .reduce((a, b) => a + b);
-  //   const agregate = { revenue: total, date: days[i] };
-  //   data.push(agregate);
-  // }
-
   const endDate = new Date();
   const startDate = new Date().getTime() - 7 * 24 * 36 * 1e5;
   let loop = new Date(startDate);

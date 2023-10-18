e28f9961aca9c40d4c472977d2782858a5015422
Mikhail <mikhailyuwankho@gmail.com>
2023-10-18 22:16:06 +0700

    POS-21 add dialog in image product

--- client/src/components/product/ProductCard.jsx
+++ client/src/components/product/ProductCard.jsx
@@ -54,7 +54,23 @@ const ProductCard = ({ product, role }) => {
           justifyContent: "center",
         }}
       >
-        <img src={product.image_url} alt={product.name} />
+        <Dialog>
+          <DialogTrigger>
+            <img src={product.image_url} alt={product.name} />
+          </DialogTrigger>
+          <DialogContent className="flex gap-2 p-4 bg-gray-50">
+            <img src={product.image_url} alt={product.name} />
+            <div className="flex p-2 flex-grow flex-col justify-center">
+              <span className="text-lg text-black">Name : {product.name}</span>
+              <span className="text-lg text-black">
+                Price : {product.price}
+              </span>
+              <span className="text-lg text-black">
+                Description : {product.description}
+              </span>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="justify-between items-center px-1">
         <div className="flex justify-between items-start">
